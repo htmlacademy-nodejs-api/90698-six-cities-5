53417235a513e5bf2f0f3727e3cda4c9acc27d34
vertina61 <natalya.vertieva@mail.ru>
2023-09-29 22:48:13 +0300

    Добавили тип MockServerData

---
+++ src/shared/types/mock-server-data.type.ts
@@ -0,0 +1,12 @@
+export type MockServerData = {
+  titles: string[];
+  descriptions: string[];
+  cities: string[];
+  previews: string[];
+  images: string[];
+  types: string[];
+  comforts: string[];
+  firstname: string[];
+  emails: string[];
+  avatarsPath: string[];
+};
