874c395a6541408b31da37254183a536a493c057
vertina61 <natalya.vertieva@mail.ru>
2023-10-06 12:12:15 +0300

    Расширили интерфейс UserDocument

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -1,7 +1,10 @@
 import { Schema, Document, model } from 'mongoose';
 import { User } from '../../types/index.js';
 
-export interface UserDocument extends User, Document {}
+export interface UserDocument extends User, Document {
+  createdAt: Date,
+  updatedAt: Date,
+}
 
 const userSchema = new Schema({
   firstname: String,
@@ -9,6 +12,6 @@ const userSchema = new Schema({
   avatarPath: String,
   password: String,
   userType: String,
-});
+}, { timestamps: true });
 
 export const UserModel = model<UserDocument>('User', userSchema);
