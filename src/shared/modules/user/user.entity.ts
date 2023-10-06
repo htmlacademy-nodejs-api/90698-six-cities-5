11fd2925e7a374a1c3c6ce2cae7f786276e62904
vertina61 <natalya.vertieva@mail.ru>
2023-10-06 14:02:01 +0300

    Добавим определение модели и схемы

--- src/shared/modules/user/user.entity.ts
+++ src/shared/modules/user/user.entity.ts
@@ -1,9 +1,21 @@
+import { getModelForClass, prop } from '@typegoose/typegoose';
 import { User } from '../../types/index.js';
 
 export class UserEntity implements User {
+  @prop({ required: true })
   public firstname: string;
+
+  @prop({ unique: true, required: true })
   public email: string;
+
+  @prop({ required: false, default: '' })
   public avatarPath: string;
+
+  @prop({ required: true })
   public password: string;
+
+  @prop({ required: true })
   public userType: string;
 }
+
+export const UserModel = getModelForClass(UserEntity);
