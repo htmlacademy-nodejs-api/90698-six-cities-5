73256d6fc021b00cb3f8cf07b2578c7a2a51ade3
vertina61 <natalya.vertieva@mail.ru>
2023-10-19 14:44:21 +0300

    Обновили интерфейс для Offers

--- src/shared/modules/offer/default-offer.service.ts
+++ src/shared/modules/offer/default-offer.service.ts
@@ -1,10 +1,12 @@
 import { inject, injectable } from 'inversify';
 import { OfferService } from './offer-service.interface.js';
-import { Component } from '../../types/index.js';
+import { Component, SortType } from '../../types/index.js';
 import { Logger } from '../../libs/logger/index.js';
 import { DocumentType, types } from '@typegoose/typegoose';
 import { OfferEntity } from './offer.entity.js';
 import { CreateOfferDto } from './dto/create-offer.dto.js';
+import { UpdateOfferDto } from './dto/update-offer.dto.js';
+import { DEFAULT_OFFER_COUNT } from './offer.constant.js';
 
 @injectable()
 export class DefaultOfferService implements OfferService {
@@ -21,6 +23,92 @@ export class DefaultOfferService implements OfferService {
   }
 
   public async findById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
-    return this.offerModel.findById(offerId).exec();
+    return this.offerModel
+      .findById(offerId)
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async find(count?: number): Promise<DocumentType<OfferEntity>[]> {
+    const limit = count ?? DEFAULT_OFFER_COUNT;
+    return this.offerModel
+      .find({limit})
+      .sort({ createdAt: SortType.Down })
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndDelete(offerId)
+      .exec();
+  }
+
+  public async updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, dto, {new: true})
+      .populate(['userId'])
+      .exec();
+  }
+
+  public async exists(documentId: string): Promise<boolean> {
+    return (await this.offerModel
+      .exists({_id: documentId})) !== null;
+  }
+
+  public async incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .findByIdAndUpdate(offerId, {'$inc': {
+        commentCount: 1,
+      }}).exec();
+  }
+
+
+  public async updateRating(offerId: string): Promise<DocumentType<OfferEntity> | null> {
+    return this.offerModel
+      .aggregate([
+        {
+          $match: {
+            $expr: {
+              $eq: [
+                '$_id',
+                {
+                  $toObjectId: offerId,
+                },
+              ],
+            },
+          },
+        },
+        {
+          $lookup: {
+            from: 'comments',
+            let: {
+              offerId: '$_id',
+            },
+            pipeline: [
+              {
+                $match: {
+                  $expr: {
+                    $eq: ['$$offerId', '$offerId'],
+                  },
+                },
+              },
+            ],
+            as: 'comments',
+          },
+        },
+        {
+          $set: {
+            rating: {
+              $avg: '$comments.rating',
+            },
+          },
+        },
+        {
+          $unset: 'comments',
+        },
+      ])
+      .exec()
+      .then(([result]) => result ?? null);
   }
 }
