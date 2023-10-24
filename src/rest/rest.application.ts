6260f752bed09dd65bfb5debca9cc89c567e15cd
vertina61 <natalya.vertieva@mail.ru>
2023-10-24 14:23:15 +0300

    Добавили создание и запуск сервера express

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,19 +1,23 @@
 import { inject, injectable } from 'inversify';
+import express, { Express } from 'express';
 import { Logger } from '../shared/libs/logger/index.js';
 import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { Component } from '../shared/types/index.js';
 import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
-import { OfferService } from '../shared/modules/offer/offer-service.interface.js';
 
 @injectable()
 export class RestApplication {
+  private server: Express;
+
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
-    @inject(Component.OfferService) private readonly offerService:OfferService,
-  ) {}
+
+  ) {
+    this.server = express();
+  }
 
   private async _initDb() {
     const mongoUri = getMongoURI(
@@ -27,15 +31,21 @@ export class RestApplication {
     return this.databaseClient.connect(mongoUri);
   }
 
+  private async _initServer() {
+    const port = this.config.get('PORT');
+    this.server.listen(port);
+  }
+
   public async init() {
     this.logger.info('Application initialization');
-    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
 
     this.logger.info('Init database…');
     await this._initDb();
     this.logger.info('Init database completed');
 
-    const result = await this.offerService.incCommentCount('6524ee556a3909e6a88e62fb');
-    console.log(result);
+    this.logger.info('Try to init server…');
+    await this._initServer();
+    this.logger.info(`🚀 Server started on http://localhost:${this.config.get('PORT')}`);
+
   }
 }
