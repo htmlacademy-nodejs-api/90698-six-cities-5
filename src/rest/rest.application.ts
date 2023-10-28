665dd4705d263d2c77c4a9a0d6bdefffe04e582f
vertina61 <natalya.vertieva@mail.ru>
2023-10-28 22:12:02 +0300

    Добавим контроллер UserController

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -17,6 +17,7 @@ export class RestApplication {
     @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
     @inject(Component.OfferController) private readonly offerController: Controller,
     @inject(Component.ExceptionFilter) private readonly appExceptionFilter: ExceptionFilter,
+    @inject(Component.UserController) private readonly userController: Controller,
 
   ) {
     this.server = express();
@@ -41,6 +42,7 @@ export class RestApplication {
 
   private async _initControllers() {
     this.server.use('/offers', this.offerController.router);
+    this.server.use('/users', this.userController.router);
   }
 
   private async _initMiddleware() {
