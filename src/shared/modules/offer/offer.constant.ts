cbd4f457d160e2489fb0e5fea31ff383ebe53fe5
vertina61 <natalya.vertieva@mail.ru>
2023-11-16 10:00:09 +0300

    Внесли правки по замечаниям

--- src/shared/modules/offer/offer.constant.ts
+++ src/shared/modules/offer/offer.constant.ts
@@ -1,12 +1,29 @@
 export const DEFAULT_OFFER_COUNT = 60;
 export const PREMIUM_OFFER_AMOUNT = 3;
-export const MIN_TITLE = 10;
-export const MAX_TITLE = 100;
-export const MIN_DESCRIPTION = 20;
-export const MAX_DESCRIPTION = 1024;
-export const MIN_ROOM = 1;
-export const MAX_ROOM = 8;
-export const MIN_GUEST = 1;
-export const MAX_GUEST = 10;
-export const MIN_PRICE = 100;
-export const MAX_PRICE = 100000;
+export const IMAGES_COUNT = 6;
+
+export const TITLE = {
+  MIN: 10,
+  MAX: 100,
+};
+
+export const DESCRIPTION = {
+  MIN: 20,
+  MAX: 1024,
+};
+
+export const ROOM = {
+  MIN: 1,
+  MAX: 8,
+};
+
+export const GUEST = {
+  MIN: 1,
+  MAX: 10,
+};
+
+
+export const PRICE = {
+  MIN: 100,
+  MAX: 100000,
+};
